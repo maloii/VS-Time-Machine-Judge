33933c75c78ff3bb836f035c2f1212529eef7de1
maloii <maloii@bk.ru>
2019-02-02 11:57:00 +0300

    First commit

---
+++ src/main/java/com/vstimemachine/judge/JudgeApplication.java
@@ -0,0 +1,14 @@
+package com.vstimemachine.judge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JudgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JudgeApplication.class, args);
+	}
+
+}
+
